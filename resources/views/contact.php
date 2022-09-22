a40c99f0dedc67d988c8545d2466f49bead9033b
utsavanand01 <anandutsav456@gmail.com>
2022-09-22 08:25:39 +0530

    first commit

---
+++ resources/views/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1st day work</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+ rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand lg navbardark bg-success">
+        <div class="container">
+        <a href=""class="navbar-brand">Laravel</a>
+        </div>
+    </nav>
+    <h1 style="text-align:center">this is contact page</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::view("/","home");
+Route::view("/about","about");
+Route::view("/contact","contact");
